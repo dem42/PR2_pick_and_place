689c551aed06a8c45d7563d93faacbb943693810
martin papanek <martin.papanek@gmail.com>
2017-09-11 17:04:29 -0700

    writeup and final code fixes

--- pr2_robot/scripts/project_fp.py
+++ pr2_robot/scripts/project_fp.py
@@ -25,9 +25,6 @@ from rospy_message_converter import message_converter
 import yaml
 import os
 
-from std_srvs.srv import Empty
-from math import pi
-from sensor_msgs.msg import JointState
 
 # Helper function to get surface normals
 def get_normals(cloud):
@@ -50,110 +47,95 @@ def send_to_yaml(yaml_filename, dict_list):
     with open(yaml_filename, 'w') as outfile:
         yaml.dump(data_dict, outfile, default_flow_style=False)
 
-def save_pcd(cloud, filename):
-    if not os.path.isfile(filename):
-        pcl.save(cloud, filename)
-        
 # Callback function for your Point Cloud Subscriber
 def pcl_callback(pcl_msg):
-    global table_cluster
-   
-    # Convert ROS msg to PCL data
+
+# Exercise-2 TODOs:
+
+    # TODO: Convert ROS msg to PCL data
     cloud = ros_to_pcl(pcl_msg)
-    save_pcd(cloud, 'original.pcd')
     
-    # Statistical Outlier Filtering
+    # TODO: Statistical Outlier Filtering
     outlier_filter = cloud.make_statistical_outlier_filter()
-    outlier_filter.set_mean_k(25)
-    outlier_filter.set_std_dev_mul_thresh(1)
+    outlier_filter.set_mean_k(50)
+    outlier_filter.set_std_dev_mul_thresh(0.9)
     cloud_no_outliers = outlier_filter.filter()
 
-    rospy.loginfo("Completed outlier filtering. Points in cloud: {}".format(cloud_no_outliers.size))
-    save_pcd(cloud_no_outliers, 'no_outlier.pcd')
     
-    # Voxel Grid Downsampling
+    # TODO: Voxel Grid Downsampling
     vox = cloud_no_outliers.make_voxel_grid_filter()
     # voxel size (also called leafs)
     LEAF_SIZE = 0.01
     vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
     cloud_downsampled = vox.filter()
 
-    rospy.loginfo("Completed downsampling. Points in cloud: {}".format(cloud_downsampled))
-    save_pcd(cloud_downsampled, 'downsampled.pcd')
-    
-    # PassThrough Filter
+    # TODO: PassThrough Filter
     passthrough_z = cloud_downsampled.make_passthrough_filter()
     filter_axis = 'z'
     passthrough_z.set_filter_field_name(filter_axis)
     axis_min = 0.60
-    axis_max = 4
+    axis_max = 1.1
     passthrough_z.set_filter_limits(axis_min, axis_max)
     cloud_filtered = passthrough_z.filter()
 
-    rospy.loginfo("Completed passthrough. Points in cloud: {}".format(cloud_filtered))
-    save_pcd(cloud_filtered, 'passthrough.pcd')
-    
-    # TODO: RANSAC Plane Segmentation
-    seg = cloud_filtered.make_segmenter()
-    seg.set_model_type(pcl.SACMODEL_PLANE)
-    seg.set_method_type(pcl.SAC_RANSAC)    
-    max_distance = 0.01
-    seg.set_distance_threshold(max_distance)    
-    inliers, coefficients = seg.segment()
-
-    # Extract inliers and outliers
-    cloud_table = cloud_filtered.extract(inliers, negative=False)
-    cloud_objects_pre = cloud_filtered.extract(inliers, negative=True)
-
-    passthrough_y = cloud_objects_pre.make_passthrough_filter()
+    passthrough_y = cloud_filtered.make_passthrough_filter()
     filter_axis = 'y'
     passthrough_y.set_filter_field_name(filter_axis)
     axis_min = -0.5
     axis_max = 0.5
     passthrough_y.set_filter_limits(axis_min, axis_max)
-    cloud_objects = passthrough_y.filter()
+    cloud_filtered = passthrough_y.filter()
+
+    if not os.path.isfile('test.pcd'):
+        pcl.save(cloud_filtered, 'test.pcd')
     
-    rospy.loginfo("Completed ransac")
-    save_pcd(cloud_table, 'table.pcd')
-    save_pcd(cloud_objects_pre, 'objects_pre.pcd')
-    save_pcd(cloud_objects, 'objects.pcd')
+    # TODO: RANSAC Plane Segmentation
+    seg = cloud_filtered.make_segmenter()
+    seg.set_model_type(pcl.SACMODEL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)    
+    max_distance = 0.02
+    seg.set_distance_threshold(max_distance)    
+    inliers, coefficients = seg.segment()
+
+    # TODO: Extract inliers and outliers
+    cloud_table = cloud_filtered.extract(inliers, negative=False)
+    cloud_objects = cloud_filtered.extract(inliers, negative=True)
     
-    # Euclidean Clustering
+    # TODO: Euclidean Clustering
     white_cloud = XYZRGB_to_XYZ(cloud_objects)
     kd_tree = white_cloud.make_kdtree()
     ec = white_cloud.make_EuclideanClusterExtraction()
     ec.set_ClusterTolerance(0.03)
-    ec.set_MinClusterSize(50)
+    ec.set_MinClusterSize(5)
     ec.set_MaxClusterSize(2200)
     
     ec.set_SearchMethod(kd_tree)
     cluster_indices = ec.Extract()
 
-    # Create Cluster-Mask Point Cloud to visualize each cluster separately
+    # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
     cluster_color = get_color_list(len(cluster_indices))
     color_cluster_point_list = []
     for j, indices in enumerate(cluster_indices):
         for i, indice in enumerate(indices):
             color_cluster_point_list.append([white_cloud[indice][0], white_cloud[indice][1], white_cloud[indice][2], rgb_to_float(cluster_color[j])])
 
-    # Create new cloud containing all clusters, each with unique color
+    #Create new cloud containing all clusters, each with unique color
     cluster_cloud = pcl.PointCloud_PointXYZRGB()
     cluster_cloud.from_list(color_cluster_point_list)
             
-    # Convert PCL data to ROS messages
+    # TODO: Convert PCL data to ROS messages
     ros_cloud_table = pcl_to_ros(cloud_table)
     ros_cloud_objects = pcl_to_ros(cloud_objects)
     ros_cluster_cloud = pcl_to_ros(cluster_cloud)
     
     rospy.loginfo("Publishing results")
     
-    # Publish ROS messages
+    # TODO: Publish ROS messages
     pcl_objects_pub.publish(ros_cloud_objects)
     pcl_table_pub.publish(ros_cloud_table)
     pcl_cluster_pub.publish(ros_cluster_cloud)
 
-    rospy.loginfo("Completed euclidean clustering")
-    save_pcd(cluster_cloud, 'cluster_cloud.pcd')
+# Exercise-3 TODOs:
 
     # Classify the clusters! (loop through each detected cluster one at a time)
     detected_objects_labels = []
@@ -194,74 +176,57 @@ def pcl_callback(pcl_msg):
     # Suggested location for where to invoke your pr2_mover() function within pcl_callback()
     # Could add some logic to determine whether or not your object detections are robust
     # before calling pr2_mover()
-
-    # setup table cloud
-    # we keep appending newly discovered table points to the table points we have already found
-    table_cluster = table_cluster + cloud_table.to_list()
-    pcl_table_cluster = pcl.PointCloud_PointXYZRGB()
-    pcl_table_cluster.from_list(table_cluster)
-    clear_collision_map()
-    collision_map_pub.publish(pcl_to_ros(pcl_table_cluster))
     
     try:
-       pr2_mover(detected_objects, table_cluster)
+       pr2_mover(detected_objects)
     except rospy.ROSInterruptException:
        pass
 
 # function to load parameters and request PickPlace service
-def pr2_mover(object_list, table_point_cloud):
-    
-    if not is_turning_done:
-        return
-    
-    # Initialize variables
-    # the dict_cloud_by_label and dict_centroid_by_label are mappings of point_cloud and centroid indexed label
-    # these are used to fetch the data for a detected object
+def pr2_mover(object_list):
+
+    # TODO: Initialize variables
     labels = []
     centroids = []
-    dict_centroid_by_label = {}
-    dict_cloud_by_label = {}
+    obj_map = {}
     for object in object_list:
         labels.append(object.label)
-        points = ros_to_pcl(object.cloud)
-        points_arr = points.to_array()
-        # Get the PointCloud for a given object and obtain it's centroid
+        points_arr = ros_to_pcl(object.cloud).to_array()
+        # TODO: Get the PointCloud for a given object and obtain it's centroid
         centroid = np.mean(points_arr, axis=0)[:3]
         centroids.append(centroid)
-        dict_centroid_by_label[object.label] = centroid
-        dict_cloud_by_label[object.label] = points
+        obj_map[object.label] = centroid
+        #np.asscalar(centroids)
 
-    # Get/Read parameters for which objects are to be picked up and where the boxes are
+    # TODO: Get/Read parameters
     object_list_param = rospy.get_param('/object_list')
     dropbox_param = rospy.get_param('/dropbox')
 
-    # Parse parameters into individual variable
+    # TODO: Parse parameters into individual variable
     dropbox_left = dropbox_param[0]['position']
     dropbox_right = dropbox_param[1]['position']
     
+    # TODO: Rotate PR2 in place to capture side tables for the collision map
+
     dict_list = []
     # TODO: Loop through the pick list
     for i in range(0, len(object_list_param)):
-
-        rospy.loginfo("picking up {}".format(i))
         
         # TODO: Create 'place_pose' for the object
         test_scene_num = Int32()
-        test_scene_num.data = 3
+        test_scene_num.data = 1
 
         object_name = String()
         object_name.data = object_list_param[i]['name']
 
-        if object_name.data not in dict_centroid_by_label:
+        if object_name.data not in obj_map:
             rospy.loginfo("Couldnt find {}".format(object_name.data))
             continue
-
-        del dict_cloud_by_label[object_name.data]
             
         pick_pose = Pose()
-        pick_pose.position.x = np.asscalar(dict_centroid_by_label[object_name.data][0])
-        pick_pose.position.y = np.asscalar(dict_centroid_by_label[object_name.data][1])
-        pick_pose.position.z = np.asscalar(dict_centroid_by_label[object_name.data][2])
+        pick_pose.position.x = np.asscalar(obj_map[object_name.data][0])
+        pick_pose.position.y = np.asscalar(obj_map[object_name.data][1])
+        pick_pose.position.z = np.asscalar(obj_map[object_name.data][2])
 
         # TODO: Assign the arm to be used for pick_place
         arm_name = String()
@@ -282,16 +247,6 @@ def pr2_mover(object_list, table_point_cloud):
         # Wait for 'pick_place_routine' service to come up
         rospy.wait_for_service('pick_place_routine')
         rospy.loginfo("Pick place routine started")
-
-        # Update the collision map (first clear it)
-        clear_collision_map()
-        collision_points = table_point_cloud
-        for (key, points) in dict_cloud_by_label.items():
-            collision_points = collision_points + points.to_list()
-        collision_cloud = pcl.PointCloud_PointXYZRGB()
-        collision_cloud.from_list(collision_points)
-        collision_map_pub.publish(pcl_to_ros(collision_cloud))
-        rospy.loginfo("collision map published")
         
         try:
             pick_place_routine = rospy.ServiceProxy('pick_place_routine', PickPlace)
@@ -308,48 +263,11 @@ def pr2_mover(object_list, table_point_cloud):
     send_to_yaml("result.yaml", dict_list)
 
 
-def jointCheck(jointState):
-    global expect_idx, next_move, expect_world, is_turning_done
-    
-    curWorldState = jointState.position[-1]
-    #rospy.loginfo("checking with {} and value {} and {}".format(expect_idx, curWorldState, expect_world[expect_idx]))
-    if abs(curWorldState - expect_world[expect_idx]) < 1e-4:
-        nextMov = next_move[expect_idx]
-        rospy.loginfo("updating to next state {}".format(nextMov))
-        if (nextMov == -100):
-            joint_sub.unregister()
-            is_turning_done = True
-        else:
-            robot_hip_joint.publish(nextMov)
-            expect_idx = expect_idx+1 
-
-def clear_collision_map():
-    rospy.wait_for_service("/clear_octomap")
-    collision_map_clearer = rospy.ServiceProxy('/clear_octomap', Empty)
-    try:
-        collision_map_clearer()
-    except rospy.ServiceException as exc: 
-        rospy.loginfo("failed with {}".format(str(exc)))
-
 if __name__ == '__main__':
     # TODO: ROS node initialization
     rospy.init_node("clustering", anonymous = True)
 
-    #Load model from disk
-    model = pickle.load(open("model.sav", "rb"))
-    clf = model["classifier"]
-    encoder = LabelEncoder()
-    encoder.classes_ = model["classes"]
-    scaler = model["scaler"]
-    
-    # Initialize variables
-    get_color_list.color_list = []
-    table_cluster = []
-    rospy.loginfo("Variables setup completed")
-
-    # collision map clearer
-    
-    # initialize scene perception callback
+    # TODO: Create Subscribers
     pcl_sub = rospy.Subscriber("/pr2/world/points", pc2.PointCloud2, pcl_callback, queue_size = 1)
 
     # TODO: Create Publishers
@@ -360,28 +278,18 @@ if __name__ == '__main__':
     object_markers_pub = rospy.Publisher("/object_markers", Marker, queue_size = 1)
     detected_objects_pub = rospy.Publisher("/detected_objects", DetectedObjectsArray, queue_size = 1)
 
-    collision_map_pub = rospy.Publisher("/pr2/3d_map/points", pc2.PointCloud2, queue_size = 1)
-    robot_hip_joint = rospy.Publisher("/pr2/world_joint_controller/command", Float64, queue_size = 10)
-        
+    #Load model from disk
+    model = pickle.load(open("model.sav", "rb"))
+    clf = model["classifier"]
+    encoder = LabelEncoder()
+    encoder.classes_ = model["classes"]
+    scaler = model["scaler"]
+    
+    # Initialize color_list
+    get_color_list.color_list = []
+
     rospy.loginfo("Clusering node setup completed")
-        
-    rate = rospy.Rate(2)
-    rate.sleep()
-
-    is_turning_done = False
-    expect_world = [0, pi/2, -pi/2, 0]
-    next_move = [pi/2, -pi/2, 0, -100]
-    expect_idx = 0
-    # Rotate PR2 in place to capture side tables for the collision map
-    # initialize turning with scene perception callback already in place
-    joint_sub = rospy.Subscriber("/pr2/joint_states", JointState, jointCheck, queue_size = 1)
-
-    busy_wait = rospy.Rate(1)
-    while not is_turning_done:
-        busy_wait.sleep()
-
-    rospy.loginfo("turning is done")
-        
+    
     # TODO: Spin while node is not shutdown
     while not rospy.is_shutdown():
         rospy.spin()
---
+++ writeup.md
@@ -0,0 +1,87 @@
+## Project: Perception Pick & Place
+
+[//]: # (Image References)
+
+[image1]: ./images/original.jpg
+[image2]: ./images/no_outlier.jpg
+[image3]: ./images/downsampled.jpg
+[image4]: ./images/passthrough.jpg
+[image5]: ./images/table.jpg
+[image6]: ./images/objects.jpg
+[image7]: ./images/cluster.jpg
+[image8]: ./images/confusion.jpg
+[image9]: ./images/labels1.jpg
+[image10]: ./images/labels2.jpg
+[image11]: ./images/lables3.jpg
+[image13]: ./images/collision_map_table_2.jpg
+[image14]: ./images/collision_map_full.jpg
+[image15]: ./images/pick_place.jpg
+
+### Writeup
+In this project I implemented the perception pipeline, the collision map construction and the pick and place robot task for the three test worlds. The following writeup is split into three tasks based on the rubric criteria, a section that discusses the pick and place challenge and a section that discusses details of the code implementation.
+
+#### Task 1: Complete Exercise 1 steps. Pipeline for filtering and RANSAC plane fitting implemented.
+I constructed the perception pipeline by consecutively applying filtering, segmentation and detection algorithms in the `pcl_callback` method. The initial input to the perception pipeline is the output of an RGB-D camera which includes color and depth information making it possible to create a point cloud of points with color in 3D space. The perception algorithms in the pipeline operate on these point clouds. The original cloud looks like this
+
+![original cloud][image1]
+
+The first step in the pipeline is to clean up the initial point cloud, removing random noise using the statistical outlier filter. I obtained the parameters for the k-means and threshold by starting off with the values from the lesson and then through trial and error attempting to improve the result.
+
+![outlier filtering][image2]
+
+The second step is to perform voxel downsampling to reduce the number of points in the point cloud to make the algorithms run faster. 
+
+![voxel downsampling][image3]
+
+The next step is to limit the range along the z axis since only the points of interest lie in a limited region. After removing the table post RANSAC, I also limited the range along the y axis to be between -0.5 and 0.5 since all objects are within this range.
+
+![passthrough filtering][image4]
+
+As the fourth step, I applied the RANSAC algorithm which is a consensus based algorithm that can locate objects with known shape in the point cloud. The goal of this RANSAC is to locate plane surfaces, because we know this surface is the table. Once we know which points in the point cloud correspond to the table, we can remove these points and what is left over in the point cloud are the objects that we are interested in. The following images show the located table and the leftover point cloud points which are the objects.
+
+![table][image5]
+![objects][image6]
+
+#### Task 2: Complete Exercise 2 steps: Pipeline including clustering for segmentation implemented.
+The next step is to apply a segmentation algorithm to determine which point cloud points belong to the same object. For this, I used the Euclidean clustering algorithm. The main difficulty here was finding the correct parameters through trial and error. I received the best results using a cluster size between 50 and 2200 points and a cluster tolerance of 0.03. This means I will only continue adding points to the cluster if they are very close which seems to result in nicely split clusters. The result of the clustering, once the parameters are correct, is a cluster per object which I color with a different color per cluster.
+
+![cluster][image7]
+
+#### Task 3: Complete Exercise 3 Steps. Features extracted and SVM trained. Object recognition implemented.
+Given the clusters of point clouds points, the robot needs to know which cluster is which object so that it can pick them up in the correct order from `pick_list.yaml`. To do this we train a Support Vector Machine using randomly generated poses of the objects we are looking for. This generation step is done by running `capture_features.py`. The generated features use the `compute_color_histograms` and `compute_normal_histograms` in `features.py` to generate a concatenated vector for every generated pose. These vectors are used to train the SVM and are stored in `trainig_set.sav`. The SVM is created from the training set using `train_svm.py` and stored in `model.sav`. 
+
+I customized the feature generation, because the number of training samples generated was too low to give good results. I increased the number of samples per object to 100. Additionally, I computed the color histogram over the HSV (hue value saturation) space instead of RGB, which makes it more resilent to lighting changes. My training confusion matrix looks as follows:
+
+![confusion matrix][image8]
+
+From the confusion matrix it's obvious that my classifier has difficulty with `glue` objects. Many objects misclasify as clue, especially `book` objects. In fact, glue is the object that is not recognized correctly in `test3.world`. My classification results are as follows:
+
+![labels 1][image9]
+![labels 2][image10]
+![labels 3][image11]
+
+As can be seen from the images, in the first two `test.world` files I classify all objects correctly. In `test3.world` I classify 7/8 correctly. The only incorrect one is the glue, which seems to be due to the glue being behind other objects. When other objects are picked up, then the glue is classified correctly.
+
+#### Pick and place challenge
+Given the labeled objects, in the method `pr2_mover`, I implemented the mover by creating dictionary objects which return the point cloud and the centroid of the point cloud for a given label. In the code these are called `dict_cloud_by_label` and `dict_centroid_by_label`. Then I load in the pick list from the parameter server and iterate through the labels in the pick list. For each label I fetch the centroid and from it I compute the PR2 arm and the target box and I issue the `pick_place_routine` service request. In addition to sending the service requests I also store the message in the `output.yaml` files which are provided in the repository.
+
+For the pick and place to work correctly, I additionally send a collision map to the PR2 robot. I compute this collision map in two steps. First inside the `__main__` part I start a left, right rotation of the PR2 robot so that I have collision data for the left and right sides of the table which are normally not visible. This rotation is implemented in the method `jointCheck`. The idea is that I have to keep a state machine which knows which rotation I need to do next. To determine whether a state transition needs to happen, I query the `/pr2/joint_states` topic. 
+
+While the PR2 is rotating, it keeps updating a global list of table point cloud points called `table_cluster`. After the rotation is done, this list stores all the table points. Inside the method `pr2_mover` we then fill this list with the remaining objects on the table and send it to the topic `/pr2/3d_map/points` which is where the collision map is stored. I have the additional method `clear_collision_map()` which is needed because the collision map in PR2 is additive so if I want to make sure I am not sending data about already picked up object, I need to send a request to the `/clear_octomap` service, which will clear the exisiting map. 
+
+![collision map table][image12]
+![collision map with objects where some have been picked up][image13]
+![pick and place][image14]
+
+#### Implementation
+I implemented everything as just one ROS node, using the `clustering` node from the project template. I noticed that topics sometimes will not receive messages if I publish a message immediately after creating a publisher, so I added a short timeout of two seconds implemented using `rospy.Rate`. 
+
+As discussed in the pick and place section, I used a state machine implemented using arrays to setup the PR2 robot world joint rotation. This involves listening to a joint state topic and switching to a new state based on what is published on that topic. I found this approach quite ugly, but I couldn't find a better way to communicate with the PR2 to see if it finished moving. There was no ROS service that I could find.
+
+The collision map calculation is implemented in a very expensive way and it only keeps track of the table and not the boxes. In the instructions it said to create a map for the table only which is what I did, however I think the correct thing to do is to also include the boxes in the collision map. It is expensive because I store the point cloud points all in a list which will contain many duplicates.
+
+The pick and place action sometimes fails because the gripper does not attach correctly. I do not think it is due to the ROS node code.
+
+Another problem with my implementation is that once an item has been picked up, the robot no longer knows the collision size of the picked-up item. This means it doesn't always manage to place them into the box because it doesn't know how it will fit. This seems to happen mainly with the large objects like the "snacks".
+
+
